16d58d95e5d99865b35eadcd9401edec29bbb91a
PyFulken <joao_leo1@hotmail.com>
2020-10-30 15:10:52 +0000

    Mongo Connection Added

--- test.py
+++ test.py
@@ -1,9 +1,17 @@
 import tkinter
 from tkinter import *
 from tkinter import ttk
+import pymongo
 import sys
 import platform
 import psutil
+import requests
+
+
+mongo = pymongo.MongoClient("mongodb+srv://root:Anna1Anna2Anna3@stocktracker.qgcja.mongodb.net/stock?retryWrites=true&w=majority")
+prints = mongo["print"]
+prints.insert_one({"name": "Peter", "address": "Lowstreet 27"})
+
 
 root = Tk()
 
@@ -13,6 +21,7 @@ screen_height = root.winfo_screenheight() - 100
 screen_width = root.winfo_screenwidth() - 100
 root.geometry("{}x{}".format(screen_width,screen_height))
 root.resizable(width="False", height="False")
+
 
 disk_label = ttk.Label(root, text="Disk Monitoring", justify="center", font="arial 20 bold", wrap=350)
 disk_label.pack(ipadx=20)
