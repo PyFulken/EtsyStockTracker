6c978a13eb9372aeefb58147a69db76202755202
PyFulken <joao_leo1@hotmail.com>
2020-11-03 16:58:42 +0000

    I still can't get the amount out of the Entry box

--- test.py
+++ test.py
@@ -12,6 +12,14 @@ connect = connect_to_db()
 # add_stock(connect["prints"], "Stamps", 10)
 all_items_amounts = get_all_stocks(connect)
 
+def getvalue(entry_var):
+    amount = entry_var.get()
+    return amount
+
+def printstuff(n, m, b):
+    print(n)
+    print(m)
+    print(b)
 #GUI
 root = Tk()
 
@@ -30,6 +38,7 @@ root.resizable(width="False", height="False")
 
 #Login page
 
+
 #Current Stock
 item_iterator=0
 for item in all_items_amounts:
@@ -41,15 +50,14 @@ for item in all_items_amounts:
     item_amount_label.grid(row=item_iterator, column=2, padx=10, pady=5)
     item_amount_label.config(text="{}".format(item[2]))
 
-    amount_entry = ttk.Entry(root, width=10 )
+    amount_entry = ttk.Entry(root, width=10)
     amount_entry.grid(row=item_iterator, column=3, padx=10, pady=5)
 
-    add_partial = partial(add_stock, item[0], item[1], amount_entry.get())
-    add_button = ttk.Button(root, command=add_partial, text="Add")
+    add_button = ttk.Button(root, command=partial(printstuff, item[0], item[1], amount_entry.get()), text="Add")
     add_button.grid(row=item_iterator, column=4, padx=10, pady=5)
     
-    remve_partial = partial(remove_stock, item[0], item[1], amount_entry.get())
-    remve_button = ttk.Button(root, command=add_partial, text="Remove")
+    remve_partial = partial(remove_stock, item[0], item[1], amount_entry.get)
+    remve_button = ttk.Button(root, command=partial(printstuff, item[0], item[1], amount_entry.get()), text="Remove")
     remve_button.grid(row=item_iterator, column=5, padx=10, pady=5)
 
     item_iterator +=1
