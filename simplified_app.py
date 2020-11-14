14704b3421b6c08a5564e09fe9fa73cb26ca3ba1
PyFulken <joao_leo1@hotmail.com>
2020-11-14 20:31:36 +0000

    Some code cleanup. TODO: Refresh page

--- simplified_app.py
+++ simplified_app.py
@@ -7,15 +7,44 @@ from tkinter import ttk
 stock_data = json_handler.load_data()
 
 def stock_add(new_value, old_data, key):
-    old_data[key] += int(new_value)
-    json_handler.change_data(old_data)
-    status_label.config(text="Added {} units to {}".format(new_value, key))
+    if new_value == "":
+        status_label.config(text="Please enter a number")
+    else:
+        old_data[key] += int(new_value)
+        json_handler.change_data(old_data)
+        status_label.config(text="Added {} units to {}".format(new_value, key))
 
 
 def stock_remove(new_value, old_data, key):
-    old_data[key] -= int(new_value)
-    json_handler.change_data(old_data)
-    status_label.config(text="Removed {} units from {}".format(new_value, key))
+    if new_value == "":
+        status_label.config(text="Please enter a number")
+    else:
+        old_data[key] -= int(new_value)
+        json_handler.change_data(old_data)
+        status_label.config(text="Removed {} units from {}".format(new_value, key))
+
+def populate_window(stock_data):
+    row_manager = 0
+    entry_box_variables = {}
+
+    for key, value in stock_data.items():
+        entry_box_variables[row_manager] = StringVar()
+        stock_label = Label(stock_frame, relief="solid", borderwidth=1, background="white", width=20, height=1, anchor="w")
+        stock_label.config(text=key)
+        stock_label.grid(row=row_manager, column=0, ipadx=10, padx=10, pady=3)
+        stock_amount = Label(stock_frame, relief="solid", borderwidth=1, background="white", width=6, height=1, anchor="e")
+        stock_amount.config(text=value)
+        stock_amount.grid(row=row_manager, column=1, ipadx=10, padx=10, pady=3)
+        amount_label= Entry(stock_frame, relief="solid", borderwidth=1, background="white", width=6, textvariable=entry_box_variables[row_manager])
+        amount_label.grid(row=row_manager, column=2, ipadx=10, padx=10, pady=3)
+
+        add_button = Button(stock_frame, text="Add", command=lambda current_variable=row_manager, current_key=key: stock_add(entry_box_variables[current_variable].get(), stock_data, current_key))
+        add_button.grid(row=row_manager, column=3, ipadx=10, padx=10, pady=3)
+
+        remove_button = Button(stock_frame, text="Remove", command=lambda current_variable=row_manager, current_key=key: stock_remove(entry_box_variables[current_variable].get(), stock_data, current_key))
+        remove_button.grid(row=row_manager, column=4, ipadx=10, padx=10, pady=3)
+
+        row_manager += 1
 
 
 #GUI
@@ -33,29 +62,14 @@ title_label.pack(side="top")
 #Current Stock
 stock_frame = Frame(root)
 stock_frame.pack()
-row_manager = 0
-entry_box_variables = {}
 
-for key, value in stock_data.items():
-    entry_box_variables[row_manager] = StringVar()
-    stock_label = Label(stock_frame, relief="solid", borderwidth=1, background="white", width=20, height=1, anchor="w")
-    stock_label.config(text=key)
-    stock_label.grid(row=row_manager, column=0, ipadx=10, padx=10, pady=3)
-    stock_amount = Label(stock_frame, relief="solid", borderwidth=1, background="white", width=6, height=1, anchor="e")
-    stock_amount.config(text=value)
-    stock_amount.grid(row=row_manager, column=1, ipadx=10, padx=10, pady=3)
-    amount_label= Entry(stock_frame, relief="solid", borderwidth=1, background="white", width=6, textvariable=entry_box_variables[row_manager])
-    amount_label.grid(row=row_manager, column=2, ipadx=10, padx=10, pady=3)
-
-    add_button = Button(stock_frame, text="Add", command=lambda current_variable=row_manager, current_key=key: stock_add(entry_box_variables[current_variable].get(), stock_data, current_key))
-    add_button.grid(row=row_manager, column=3, ipadx=10, padx=10, pady=3)
-
-    remove_button = Button(stock_frame, text="Remove", command=lambda current_variable=row_manager, current_key=key: stock_remove(entry_box_variables[current_variable].get(), stock_data, current_key))
-    remove_button.grid(row=row_manager, column=4, ipadx=10, padx=10, pady=3)
+populate_window(stock_data)
 
 
-    row_manager += 1
+#Status Bar
 
 status_label = Label(root, text="Waiting...", font = "Helvetica 10 bold italic")
-status_label.pack()
+status_label.pack(side="left")
+refresh_button = Button(root, text="Refresh")
+refresh_button.pack(side="right", padx=60, pady=20)
 root.mainloop()
