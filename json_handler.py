ac180af2146b2163c93a015bfe364b74f345ba42
PyFulken <joao_leo1@hotmail.com>
2020-11-15 15:55:50 +0000

    Made the program not crash if data.json is not present

--- json_handler.py
+++ json_handler.py
@@ -1,4 +1,9 @@
 import json 
+import os.path
+
+if not os.path.isfile('./data.json'):
+    with open('data.json', 'w') as json_file:
+        json.dump({"Default Item": 5}, json_file)
 
 def load_data():
     with open('./data.json') as json_file:
