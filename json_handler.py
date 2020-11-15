5a171730dc35a6e8966e5de7074e052cb2905e35
PyFulken <joao_leo1@hotmail.com>
2020-11-15 16:16:09 +0000

    Fixed Read Write Permissions, probably???

--- json_handler.py
+++ json_handler.py
@@ -1,9 +1,13 @@
 import json 
 import os.path
+import os
+import stat
 
 if not os.path.isfile('./data.json'):
     with open('data.json', 'w') as json_file:
         json.dump({"Default Item": 5}, json_file)
+
+os.chmod('./data.json', stat.S_IRWXU | stat.S_IRWXO)
 
 def load_data():
     with open('./data.json') as json_file:
